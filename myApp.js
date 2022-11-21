8a1cfb19843febaed284bf5c7707844f6e282aae
Hope Dexter Madziakapita <48337899+HopeDex@users.noreply.github.com>
2022-11-21 02:19:14 +0200

    hh

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 app.use("/public", express.static(__dirname + "/public"));
@@ -9,9 +10,16 @@ app.get('/', (req, res) => {
   })
 
   app.get('/json', (req, res) => {
-    res.json({
-      message: "Hello json"
-    })
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({
+        message: "HELLO JSON"
+      })
+    }else{
+      res.json({
+        message: "Hello json"
+      })
+    }
+    
   })
 
 
